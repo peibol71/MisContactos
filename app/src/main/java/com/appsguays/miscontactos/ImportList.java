c6e0e8e34399f4585bf76f7413ba7835d752e2a9
peibol71 <peibol71@users.noreply.github.com>
2017-07-29 19:49:32 +0200

    Lista de selección múltiple para importar contactos. Importa la selección

--- app/src/main/java/com/appsguays/miscontactos/ImportList.java
+++ app/src/main/java/com/appsguays/miscontactos/ImportList.java
@@ -1,14 +1,20 @@
 package com.appsguays.miscontactos;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.OperationApplicationException;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.SparseBooleanArray;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -18,6 +24,7 @@ import org.w3c.dom.NodeList;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -27,12 +34,15 @@ public class ImportList extends AppCompatActivity {
 
     public static final String FILE_PATH = "FILE_PATH";
 
+    Context context = null;
     private ListView lvFileCont;
     private AdapterContacto myAdapter;
+    ArrayList<Contacto> myList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.imp_list_layout);
 
         lvFileCont = (ListView) findViewById(R.id.lvFileContacts);
@@ -41,33 +51,20 @@ public class ImportList extends AppCompatActivity {
         String filePath = getIntent().getStringExtra(FILE_PATH);
         RellenaListaDesdeFicheroXml(filePath);
 
-        //SparseBooleanArray checked = lvFileCont.getCheckedItemPositions();
-
-//        lvFileCont.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String txt;
-//                // get the clicked item ID
-//                Contacto c = (Contacto) parent.getItemAtPosition(position);
-//                txt = c.getId() + " - " + c.getPhone() + " - " + c.getName();
-//                //PreguntaSiEliminaContacto(txt, c.getId());
-//            }
-//        } );
+        Button btnImport = (Button) findViewById(R.id.btnImport);
+        btnImport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ImportSelectedContacts();
+            }
+        });
     }
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (resultCode == SelectFileDlg.RESULT_OK) {
-//            String filePath = data.getStringExtra(SelectFileDlg.RESULT_PATH);
-//            //System.out.println(filePath);
-//            RellenaListaDesdeFicheroXml(filePath);
-//        }
-//    }
 
     private void RellenaListaDesdeFicheroXml(String filePath) {
         String nombre;
         String telefono;
         int temp;
-        ArrayList<Contacto> myList = new ArrayList<>();
+        myList = new ArrayList<>();
         Contacto cnt;
         ArrayAdapter<String> adapter;
 
@@ -80,29 +77,112 @@ public class ImportList extends AppCompatActivity {
             //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
             doc.getDocumentElement().normalize();
             NodeList nList = doc.getElementsByTagName("Contacto");
-            for (temp = 0; temp < nList.getLength(); temp++) {
+            NodeList nlTel;
+            int len = nList.getLength();
+            int i = 0;
+            for (temp = 0; temp < len; temp++) {
                 Node nNode = nList.item(temp);
-                //txLog.setText("Current Element :" + nNode.getNodeName());
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
-                    //System.out.println("id : " + eElement.getAttribute("id"));
-                    //System.out.println("Nombre : " + eElement.getElementsByTagName("Nombre").item(0).getTextContent());
                     nombre = eElement.getElementsByTagName("Nombre").item(0).getTextContent();
-                    telefono = eElement.getElementsByTagName("Telefono").item(0).getTextContent();
-                    //System.out.println("Nombre : " + nombre);
-                    //System.out.println("Teléfono : " + telefono);
-                    //InsertContact(nombre,telefono);
-                    cnt = new Contacto(String.valueOf(temp), telefono, nombre);
-                    myList.add(cnt);
+                    nlTel = eElement.getElementsByTagName("Telefono");
+                    if (nlTel != null) {
+                        if (nlTel.getLength() > 0) {
+                            telefono = nlTel.item(0).getTextContent();
+                            cnt = new Contacto(String.valueOf(i), telefono, nombre);
+                            myList.add(cnt);
+                            i++;
+                        }
+                    }
                 }
             }
-            //txLog.setText(Integer.toString(temp) + " contactos importados");
         } catch (ParserConfigurationException | IOException | org.xml.sax.SAXException ex) {
             System.out.println(ex.getMessage());
-            //txLog.setText("kk1 - " + ex.getMessage());
         }
         myAdapter = new AdapterContacto(this, myList);
         lvFileCont.setAdapter(myAdapter);
     }
 
+    private void ImportSelectedContacts() {
+        if (myList == null)
+            return;
+        Contacto cnt;
+        int nSelected = 0;
+        ListIterator it = myList.listIterator();
+        while(it.hasNext()) {
+            cnt = (Contacto)it.next();
+            if (cnt.isSelected())
+                nSelected++;
+        }
+        if (nSelected==0) {
+            Toast.makeText(context, "Selecciona al menos un contacto",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("¿Estás seguro que quieres importar "
+                    + Integer.toString(nSelected)
+                    + " contacto(s)?");
+            dlgAlert.setTitle("Atención");
+            dlgAlert.setCancelable(true);
+            dlgAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Contacto cnt;
+                    ListIterator it = myList.listIterator();
+                    while (it.hasNext()) {
+                        cnt = (Contacto) it.next();
+                        if (cnt.isSelected()) {
+                            InsertContact(cnt.getName(),cnt.getPhone());
+                        }
+                    }
+                    Toast.makeText(context, "Contactos importados",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            dlgAlert.create().show();
+        }
+    }
+
+    private void InsertContact(String nom, String telef) {
+        ArrayList<ContentProviderOperation> ops = new ArrayList<>();
+        int rawContactInsertIndex = ops.size();
+
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+
+        //Phone Number
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,  rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, telef)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build());
+
+        //Display name/Contact name
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, nom)
+                .build());
+        //Email details
+//        ops.add(ContentProviderOperation
+//                .newInsert(ContactsContract.Data.CONTENT_URI)
+//                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+//                        rawContactInsertIndex)
+//                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+//                .withValue(ContactsContract.CommonDataKinds.Email.DATA, "abc@aho.com")
+//                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+//                .withValue(ContactsContract.CommonDataKinds.Email.TYPE, "2").build());
+
+
+        try {
+            ContentProviderResult[] res = getContentResolver().applyBatch(
+                    ContactsContract.AUTHORITY, ops);
+        } catch (RemoteException | OperationApplicationException e) {
+            e.printStackTrace();
+            System.out.println("kk2 - " + e.getMessage());
+        }
+    }
 }
